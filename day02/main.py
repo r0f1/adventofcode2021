306f7c275a5af67dec8696bae2206d2c12eb78c4
Florian Rohrer <semicolonator@gmail.com>
2021-12-07 23:42:04 +0100

    Day 2

--- day02/main.py
+++ day02/main.py
@@ -1,19 +1,10 @@
-
 with open("input.txt") as f:
     cmds = [(c[0], int(c[1])) for c in [x.split() for x in f]]
 
-x, d = 0, 0
+x, d1, d2, a = 0, 0, 0, 0
 for c, i in cmds:
-    if c == "down": d += i
-    elif c == "up": d -= i
-    else: x += i
+    if c == "down": d1 += i; a += i
+    elif c == "up": d1 -= i; a -= i
+    else: x += i; d2 += a*i
 
-print(x*d)
-
-x, d, a = 0, 0, 0
-for c, i in cmds:
-    if c == "down": a += i
-    elif c == "up": a -= i
-    else: x += i; d += a*i
-
-print(x*d)
+print(x*d1, x*d2)
