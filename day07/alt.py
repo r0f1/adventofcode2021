4508bf0b2505180dadb88f567c8f6e5d0fcb1907
Florian Rohrer <semicolonator@gmail.com>
2021-12-07 18:46:39 +0100

    Day 7

--- day07/alt.py
+++ day07/alt.py
@@ -1,6 +1,6 @@
 # Ideas:
 # The median minimizes the sum of absolute differences. Exactly what we need here.
-# The mean minimizes the sum of squared differences. Almost what we need here, since we have n^2*n/2 as our distances.
+# The mean minimizes the sum of squared differences. Almost what we need here, since we have (n^2+n)/2 as our distances.
 # Article: https://www.johnmyleswhite.com/notebook/2013/03/22/modes-medians-and-means-an-unifying-perspective/
 # More thorough explanation: https://www.reddit.com/r/adventofcode/comments/rawxad/2021_day_7_part_2_i_wrote_a_paper_on_todays/
 
