bf9dd800156fb7e68853fe3e72f794942fd0d083
Florian Rohrer <semicolonator@gmail.com>
2021-12-10 15:35:37 +0100

    Day 10

--- day10/main.py
+++ day10/main.py
@@ -1,49 +1,26 @@
-from lark import Lark
-from lark.exceptions import UnexpectedCharacters
+from functools import reduce
 from statistics import median
 
 with open("input.txt") as f:
     lines = [x.strip() for x in f]
 
-g = """
-l: c +
-c: (c1 | c2 | c3 | c4) *
-c1: "(" c ")"
-c2: "[" c "]"
-c3: "{" c "}"
-c4: "<" c ">"
-"""
+bs = {"(": ")", "[": "]", "{": "}", "<": ">"}
+d1 = {")": 3, "]": 57, "}": 1197, ">": 25137}
+d2 = {"(": 1, "[": 2, "{": 3, "<": 4}
 
-p = Lark(g, start="l")
 part1 = []
-remaining = []
-for l in lines:
-    try:
-        p.parse(l)
-    except UnexpectedCharacters as ex:
-        part1.append(l[ex.pos_in_stream])
-    except:
-        remaining.append(l)
-
-d = {")": 3, "]": 57, "}": 1197, ">": 25137}
-print(sum(d[s] for s in part1))
-
-def is_closing(a, b):
-    return any([a == "(" and b == ")", a == "[" and b == "]", a == "{" and b == "}", a == "<" and b == ">"])
-
-d = {"(": 1, "[": 2, "{": 3, "<": 4}
 part2 = []
-for line in remaining:
+for line in lines:
     stack = []
     for c in line:
-        if len(stack) > 0 and is_closing(stack[-1], c):
-            stack.pop()
-        else:
+        if c in "<{[(": 
             stack.append(c)
-    score = 0
-    for x in stack[::-1]:
-        score *= 5
-        score += d[x]
-    part2.append(score)
+        else:
+            if bs[stack.pop()] != c:
+                part1.append(d1[c])
+                break
+    else:
+        part2.append(reduce(lambda x, y: x*5 + d2[y], stack[::-1], 0))
 
+print(sum(part1))
 print(median(part2))
