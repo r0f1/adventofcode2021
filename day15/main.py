a4b2333ac525e55d7f674c78735d87738835c541
Florian Rohrer <semicolonator@gmail.com>
2021-12-15 10:57:15 +0100

    Day 15

--- day15/main.py
+++ day15/main.py
@@ -9,14 +9,13 @@ def solve(weights):
         G[u][v]["weight"] = weights[v[1]][v[0]]
     print(shortest_path_length(G, (0,0), (n-1,n-1), "weight"))
 
-with open("input.txt") as f:
-    risk = [[int(i) for i in list(x.strip())] for x in f]
-
+risk = np.genfromtxt("input.txt", delimiter=1, dtype=int)
 solve(risk)
 
 n = len(risk)
 arr = np.zeros((n*5, n*5), dtype=np.uint8)
 for row, col in product(range(n*5), range(n*5)):
-    arr[col][row] = (risk[col % n][row % n] - 1 + (row//n) + (col//n)) % 9 + 1
+    arr[col][row] = risk[col % n][row % n] + (row//n) + (col//n)
 
+arr[arr > 9] -= 9
 solve(arr)
