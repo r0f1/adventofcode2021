a4ee02461a068bebcd6cac40cf2b0531b30255bf
Florian Rohrer <semicolonator@gmail.com>
2021-12-17 11:17:40 +0100

    Day 17

---
+++ day17/main.py
@@ -0,0 +1,18 @@
+from itertools import product
+
+def check_velocity_valid(dx, dy, target):
+    tminx, tmaxx, tminy, tmaxy = target
+    pos_x, pos_y = 0, 0
+    while pos_x <= tmaxx and pos_y >= tminy:
+        pos_x, pos_y, dx, dy = pos_x+dx, pos_y+dy, max(0, dx-1), dy-1
+        if tminx <= pos_x <= tmaxx and tminy <= pos_y <= tmaxy:
+            return True
+    return False
+
+def highest_y(y):
+    return (y+1) * y // 2
+
+target = 150, 171, -129, -70
+velocities = [dy for dx, dy in product(range(1000), range(-1000, 1000)) if check_velocity_valid(dx, dy, target)]
+print(highest_y(max(velocities)))
+print(len(velocities))
