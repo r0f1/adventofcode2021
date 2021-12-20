6f99b1ccd66cf1ae5ff06306b9cd400a72fe3f84
Florian Rohrer <semicolonator@gmail.com>
2021-12-20 09:38:23 +0100

    Day 20

--- day20/main.py
+++ day20/main.py
@@ -4,11 +4,10 @@ from scipy.ndimage import generic_filter
 with open("input.txt") as f:
     l, img = f.read().split("\n\n")
 
-l = [0 if c == '.' else 1 for c in l]
-img = np.array([[0 if c == '.' else 1 for c in row] for row in img.strip().split("\n")], dtype="int")
+l = [int(c == '#') for c in l]
+img = np.pad(np.array([[int(c == '#') for c in r] for r in img.strip().split("\n")], dtype="int"), (51,51))
 
-img = np.pad(img, (50,50))
 for k in range(50):
     img = generic_filter(img, lambda a: l[sum(d*2**i for i, d in enumerate(a.astype(int)[::-1]))], size=3)
-    if k in (1,49):
+    if k+1 in (2,50):
         print(np.count_nonzero(img))
